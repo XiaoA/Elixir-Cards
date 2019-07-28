f0d3c4097d112a50bf52bc1629f862f78bacbb02
Andrew Buckingham <abuckingham99@gmail.com>
2019-07-27 18:58:15 -0700

    Add test that verifies the number of cards in the deck

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,4 +2,8 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "create_deck makes 62 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 52
+  end
 end
